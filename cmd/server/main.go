b6af34ad9578fb78e2e8f1ccffd71cee90d75d35
Andrey Tarasov <andrey13.tarasov@yandex.ru>
2020-11-25 18:25:56 +0300

    Добавил версию сборки

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,10 +6,16 @@ import (
 	"github.com/axard/samplekube/internal/cfg"
 	"github.com/axard/samplekube/internal/log"
 	"github.com/axard/samplekube/internal/router"
+	"github.com/axard/samplekube/internal/version"
 	"go.uber.org/zap"
 )
 
 func main() {
+	log.Logger.Info(
+		"start",
+		zap.String("verions", version.Version),
+	)
+
 	err := http.ListenAndServe(cfg.Address(), router.New())
 	if err != nil {
 		log.Logger.Fatal(
