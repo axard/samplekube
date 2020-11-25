2ef3c403991ac6bd777af1f89896e8ba02a7dc70
Andrey Tarasov <andrey13.tarasov@yandex.ru>
2020-11-25 12:33:28 +0300

    Добавил лог

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/axard/samplekube/internal/log"
+	"go.uber.org/zap"
 )
 
 func main() {
-	handler := func(w http.ResponseWriter, _ *http.Request) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Hello World!\n")
+		log.Logger.Info(
+			"New request",
+			zap.String("URL", r.URL.String()),
+			zap.String("Method", r.URL.String()),
+		)
 	}
 
 	http.HandleFunc("/", handler)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Logger.Fatal(
+			"Server failed",
+			zap.String("error", err.Error()),
+		)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/axard/samplekube
 
 go 1.14
+
+require go.uber.org/zap v1.16.0
